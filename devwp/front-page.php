4132de38a499f893b9bf9d85acbd0fce67821954
Myzwer <josh@onefortyfivedesign.com>
2020-11-09 21:54:30 -0500

    Initial Commit

---
+++ devwp/front-page.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * Template Name: Front Page
+ *
+ * The Frontpage of the Josh Theme
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Josh Theme Best Theme
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <!--Video is from RoyaltyFreeTube on Youtube, can be found here: https://www.youtube.com/watch?v=pvomDfQmhWQ
+    We did not create this video, no changes were made to the original work. -->
+    <video src="http://unfantasticgamers.local/wp-content/uploads/2020/11/UFG-BG.mp4" autoplay loop playsinline muted></video>
+    <header class="viewport-header">
+        <div class = "fullpage-hero fullWidth">
+            <h1 class = "center logo">Mom Get The Camera!</h1>
+            <div class = "button-container">
+                <button class = "featured-button center">Watch the latest disaster!</button>
+                <div class="arrow">
+                    <a href="#"></a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="grid-container fullWidth about-banner">
+        <div class="grid-x grid-margin-x">
+            <div class="cell small-12 medium-8 medium-offset-2">
+                <h1 class = "center logo alt-color">What is this?</h1>
+                <p class = "about-details">Lorem ipsum dolor amet tumblr hexagon occupy, franzen adaptogen sustainable cornhole hot chicken disrupt try-hard biodiesel neutra. Quinoa tumeric taiyaki DIY before they sold out sriracha twee lumbersexual seitan live-edge. Tote bag gochujang la croix taxidermy iceland cray adaptogen art party kombucha pickled schlitz. Four loko irony semiotics, taxidermy bushwick 90's kinfolk. Live-edge irony vexillologist YOLO, etsy ennui microdosing tbh vape gochujang ugh. Austin activated charcoal venmo tote bag. You probably haven't heard of them 3 wolf moon swag, fanny pack tote bag sartorial raw denim seitan chillwave everyday carry narwhal farm-to-table iPhone VHS.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="grid-container fullWidth featured-video">
+        <div class="grid-x grid-margin-x">
+            <div class="cell small-12">
+                <h1 class = "center logo alt-color padding-nudge">Watch our Nonsense</h1>
+                <div class="video-slider">
+
+                    <div>
+                        <div class="grid-container">
+                            <div class="grid-x grid-margin-x">
+                                <div class="cell small-12 medium-12 large-8 feat-video">
+                                    <h2 class = "center logo alt-color">Latest Video</h2>
+                                    <div class = "outer">
+                                        <img src="https://unfantasticgamers.com/wp-content/uploads/2019/12/Cant-catch-me-League-of-trash-episode-17.png" alt="">
+                                        <a id="play-video" class="popup-youtube video-play-button" href="https://www.youtube.com/watch?v=X6wGfP5vbeo">
+                                            <span></span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="cell small-12 medium-12 large-4">
+                                    <div class="grid-x">
+                                        <div class="cell small-12">
+                                            <h2 class = "center logo alt-color desktop-title-fix">Other Recent Vidoes</h2>
+                                        </div>
+                                        <div class="cell small-12 medium-6 large-12 small-video top-vid">
+                                            <div class = "outer">
+                                                <img src="https://unfantasticgamers.com/wp-content/uploads/2019/11/League-of-Trash-episode-16-Janna-in-the-wind-city-Thumbnail.jpg" alt="">
+                                                <a id="play-video" class="popup-youtube video-play-button" href="https://www.youtube.com/watch?v=utZuNHnze_I&t">
+                                                    <span></span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="cell small-12 medium-6 large-12 small-video bot-vid">
+                                            <div class = "outer">
+                                                <img src="https://unfantasticgamers.com/wp-content/uploads/2019/10/apex-s3-thumbnail.jpg" alt="">
+                                                <a id="play-video" class="popup-youtube video-play-button" href="https://www.youtube.com/watch?v=cJf3-w0f5pI">
+                                                    <span></span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="grid-container">
+                            <div class="grid-x grid-margin-x">
+                                <div class="cell small-12 medium-12 large-8 feat-video">
+                                    <h2 class = "center logo alt-color">Featured Matt Video</h2>
+                                    <div class = "outer">
+                                        <img src="https://unfantasticgamers.com/wp-content/uploads/2017/12/Thumbnail-Ep-1-1.png" alt="">
+                                        <a id="play-video" class="popup-youtube video-play-button" href="https://www.youtube.com/watch?v=GtQZ6XXt69c">
+                                            <span></span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="cell small-12 medium-12 large-4">
+                                    <div class="grid-x">
+                                        <div class="cell small-12">
+                                            <h2 class = "center logo alt-color desktop-title-fix">Other Matt Videos</h2>
+                                        </div>
+                                        <div class="cell small-12 medium-6 large-12 small-video top-vid">
+                                            <div class = "outer">
+                                                <img src="https://unfantasticgamers.com/wp-content/uploads/2019/12/maxresdefault-copy.jpg" alt="">
+                                                <a id="play-video" class="popup-youtube video-play-button" href="https://www.youtube.com/watch?v=os-yQhewRsE">
+                                                    <span></span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="cell small-12 medium-6 large-12 small-video bot-vid">
+                                            <div class = "outer">
+                                                <img src="https://unfantasticgamers.com/wp-content/uploads/2017/12/League-of-Trash-Episode-4-Sexy-Nidalee-Skins-Thumbnail-1.jpg" alt="">
+                                                <a id="play-video" class="popup-youtube video-play-button" href="https://www.youtube.com/watch?v=LG_CBq7oVrE">
+                                                    <span></span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="grid-container">
+                            <div class="grid-x grid-margin-x">
+                                <div class="cell small-12 medium-12 large-8 feat-video">
+                                    <h2 class = "center logo alt-color">Featured Josh Video</h2>
+                                    <div class = "outer">
+                                        <img src="https://unfantasticgamers.com/wp-content/uploads/2018/03/Saints-row-lets-play-thumbnail-Josh.png" alt="">
+                                        <a id="play-video" class="popup-youtube video-play-button" href="https://www.youtube.com/watch?v=iA4orV3oP-A">
+                                            <span></span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="cell small-12 medium-12 large-4">
+                                    <div class="grid-x">
+                                        <div class="cell small-12">
+                                            <h2 class = "center logo alt-color desktop-title-fix">Other Josh Videos</h2>
+                                        </div>
+                                        <div class="cell small-12 medium-6 large-12 small-video top-vid">
+                                            <div class = "outer">
+                                                <img src="https://unfantasticgamers.com/wp-content/uploads/2017/12/Ep-1-1.jpg" alt="">
+                                                <a id="play-video" class="popup-youtube video-play-button" href="https://www.youtube.com/watch?v=hAD0tSi1RAA">
+                                                    <span></span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="cell small-12 medium-6 large-12 small-video bot-vid">
+                                            <div class = "outer">
+                                                <img src="https://unfantasticgamers.com/wp-content/uploads/2017/12/Mass-Effect-3-War-Asset-Guide.jpg" alt="">
+                                                <a id="play-video" class="popup-youtube video-play-button" href="https://www.youtube.com/watch?v=5cxATjBiG-4">
+                                                    <span></span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="grid-container fullWidth featured-comments">
+        <div class="grid-x grid-margin-x">
+            <div class="cell small-12">
+                <h1 class = "center logo alt-color padding-nudge">See what others say!</h1>
+                <p class = "featured-reviews">"bro nice video very sex thank you" <span class = "author">- Rex L </span></p>
+                <p class="featured-reviews">"what mission is this"<span class = "author">- Jessica Komalestewa </span></p>
+                <p class="featured-reviews">"cool"<span class = "author">- Rosendo Rodriguez </span></p>
+                <p class="featured-reviews">"Great video man! Check out my channel if you'd like"<span class = "author">- Jazz </span></p>
+                <p class="featured-reviews">"You guys still have a Youtube?"<span class = "author">- Our Parents </span></p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+<?php get_footer();
