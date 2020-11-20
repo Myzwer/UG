a4672fa7a72c9dd97636f5cd8b80b2f0e962e765
Myzwer <josh@onefortyfivedesign.com>
2020-11-20 03:07:05 -0500

    Finished the about us page until ACF gets added later.

---
+++ devwp/about.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Template Name: About Us
+ *
+ * The About Us page of the Josh Theme
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Josh Theme Best Theme
+ * @since 1.0.0
+ */
+
+get_header(); ?>
+
+    <div class="grid-container full-width">
+        <div class="grid-x grid-padding-x full-background" style = "background: url(http://unfantasticgamers.local/wp-content/uploads/2020/11/Matt-and-Josh.jpg);  background-position: center center;">
+            <div class="large-12 cell">
+                <div class="content-middle">
+                    <h1 class = "center" >About Us</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class = "fullWidth gradient-bg-r-y padding-bottom">
+        <div class="grid-container">
+            <div class="grid-x grid-margin-x grid-margin-y">
+                <div class="small-12 cell ">
+                    <h2 class = "center add-padding">Who are we?</h2>
+                    <div class="grid-container">
+                        <div class="grid-x grid-margin-x grid-margin-y margin-bottom">
+                            <div class="small-12 medium-6 cell gradient-bg-p-p rounded box-shadow">
+                                <h2 class = "center add-padding">Josh</h2>
+                                <h4 class = "lr-padding">Web Developer</h4>
+                                <p class = "no-spacing lr-padding padding-bottom">Lorem ipsum dolor amet tumblr hexagon occupy, franzen adaptogen sustainable cornhole hot chicken disrupt try-hard biodiesel neutra. Quinoa tumeric taiyaki DIY before they sold out sriracha twee lumbersexual seitan live-edge. Tote bag gochujang la croix taxidermy iceland cray adaptogen art party kombucha pickled schlitz. Four loko irony semiotics, taxidermy bushwick 90's kinfolk. Live-edge irony vexillologist YOLO, etsy ennui microdosing tbh vape gochujang ugh. Austin activated charcoal venmo tote bag. You probably haven't heard of them 3 wolf moon swag, fanny pack tote bag sartorial raw denim seitan chillwave everyday carry narwhal farm-to-table iPhone VHS.</p>
+                            </div>
+
+                            <div class="small-12 medium-6 cell gradient-bg-p-p rounded box-shadow">
+                                <h2 class = "center add-padding ">Matt</h2>
+                                <h4 class = "lr-padding">A Chemist or Something</h4>
+                                <p class = "no-spacing lr-padding padding-bottom">Lorem ipsum dolor amet tumblr hexagon occupy, franzen adaptogen sustainable cornhole hot chicken disrupt try-hard biodiesel neutra. Quinoa tumeric taiyaki DIY before they sold out sriracha twee lumbersexual seitan live-edge. Tote bag gochujang la croix taxidermy iceland cray adaptogen art party kombucha pickled schlitz. Four loko irony semiotics, taxidermy bushwick 90's kinfolk. Live-edge irony vexillologist YOLO, etsy ennui microdosing tbh vape gochujang ugh. Austin activated charcoal venmo tote bag. You probably haven't heard of them 3 wolf moon swag, fanny pack tote bag sartorial raw denim seitan chillwave everyday carry narwhal farm-to-table iPhone VHS.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="grid-container">
+                        <div class="grid-x grid-margin-x">
+                            <h2 class = "center add-padding ">What is UG?</h2>
+                            <div class="small-12 cell gradient-bg-c-r rounded box-shadow">
+                                <p class = "add-padding">Lorem ipsum dolor amet tumblr hexagon occupy, franzen adaptogen sustainable cornhole hot chicken disrupt try-hard biodiesel neutra. Quinoa tumeric taiyaki DIY before they sold out sriracha twee lumbersexual seitan live-edge. Tote bag gochujang la croix taxidermy iceland cray adaptogen art party kombucha pickled schlitz. Four loko irony semiotics, taxidermy bushwick 90's kinfolk. Live-edge irony vexillologist YOLO, etsy ennui microdosing tbh vape gochujang ugh. Austin activated charcoal venmo tote bag. You probably haven't heard of them 3 wolf moon swag, fanny pack tote bag sartorial raw denim seitan chillwave everyday carry narwhal farm-to-table iPhone VHS.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+<?php get_footer();
