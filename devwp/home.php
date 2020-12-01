94049a13f4dab92d873a13306a08bad8ce2fd623
Myzwer <josh@onefortyfivedesign.com>
2020-12-01 09:09:19 -0500

    Began working on the videos page, working on filtering system for the categories at the moment. RIght now the trick is showing only a handful but allowing the rest to be indexable.

--- devwp/home.php
+++ devwp/home.php
@@ -14,67 +14,71 @@
 get_header(); ?>
 
     <div class="grid-container full-width">
-        <div class="grid-x grid-padding-x full-background" style = "background: url(https://images.unsplash.com/photo-1501612780327-45045538702b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80);  background-position: center center;">
-            <div class="large-12 cell">
+        <div class="grid-x grid-padding-x full-background" style = "background: linear-gradient(
+                    rgba(0, 0, 0, 0.5),
+                    rgba(0, 0, 0, 0.5)
+                    ),url(https://images.unsplash.com/photo-1511512578047-dfb367046420?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2551&q=80);
+                    background-position: center center;
+                    background-size: cover;">
+        <div class="large-12 cell">
                 <div class="content-middle">
-                    <h1 class = "center" >One Awesome Title</h1>
-                    <button class="btn btn-v1 center">Button 4</button>
+                    <h1 class = "center" >Watch Our Nonsense</h1>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="grid-container">
-        <div class="grid-x grid-padding-x">
-            <?php
-            if ( have_posts() ) :
-            while ( have_posts() ) : the_post();
-            echo "<div class='small-12 large-6 cell'>";
-            ?>
 
-            <div class = 'blog-card'>
-                <a href = "<?php the_permalink(); ?>">
+    <div class = "gradient-bg-r-y full-width">
+        <div class="grid-container">
+            <div class="grid-x grid-margin-x grid-margin-y margin-top margin-bottom">
+                <div class="small-12 center">
+                    <h2 class = "add-padding">Latest Videos</h2>
+                </div>
 
+                <div class="small-12 medium-6 medium-offset-6 center">
+                    <h3>Categories</h3>
+                    <section>
+                        <input type="text" id="filter" placeholder="Type to filter"/>
+                        <ul id="list" class="List">
+                            <li>League of Trash</li>
+                            <li>Standalone</li>
+                            <li>Dead Space</li>
+                            <li>Saints Row</li>
+                            <li>Lamar Flynn</li>
+                            <li>Jameson Davidson</li>
+                            <li>Harper Lynn</li>
+                            <li>Keith Woods</li>
+                            <li>Cally Dominguez</li>
+                            <li>Carter Gould</li>
+                            <li>Brenna Sutton</li>
+                            <li>Robert Vang</li>
+                            <li>Clare Floyd</li>
+                        </ul>
+                    </section>
+                </div>
 
-                    <?php
-                    echo "<div class = 'card-thumbnail'>";
-                    the_post_thumbnail();
-                    echo "</div>";
+                <div class="small-12 medium-6 large-4 cell video-card">
+                    <img src="http://unfantasticgamers.local/wp-content/uploads/2020/11/Cant-catch-me-League-of-trash-episode-17-copy.jpg" alt="">
+                    <h4>Video Title</h4>
+                    <p> Series:
+                        <span class = "category">League Of Trash</span></p>
+                </div>
 
-                    echo "<div class='card-cat'>";
+                <div class="small-12 medium-6 large-4 cell video-card">
+                    <img src="http://unfantasticgamers.local/wp-content/uploads/2020/11/Cant-catch-me-League-of-trash-episode-17-copy.jpg" alt="">
+                    <h4>Video Title</h4>
+                </div>
 
-                    the_tags('', ', ', '<br />');
-
-                    echo "</div>";
-
-                    echo '<h3><a href="' . get_permalink() . '">' . get_the_title() . '</a></h3>';
-
-                    the_excerpt( '<p class = "blog-excerpt">', '</p>' );
-
-
-                    echo "<div class = 'card-details'>";
-
-                    echo "<span class='card-name'>";
-                    the_author();
-                    echo "</span>";
-                    echo " | ";
-
-                    echo "<span class='card-date'>";
-                    echo get_the_date();
-                    echo "</span>";
-
-
-                    echo "</a>";
-                    echo "</div>";
-                    echo "</div>";
-                    echo "</div>";
-                    endwhile;
-                    else:
-                        _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
-                    endif;
-                    ?>
+                <div class="small-12 medium-6 large-4 cell video-card">
+                    <img src="http://unfantasticgamers.local/wp-content/uploads/2020/11/Cant-catch-me-League-of-trash-episode-17-copy.jpg" alt="">
+                    <h4>Video Title</h4>
+                </div>
             </div>
         </div>
+    </div>
+
+   
 
 
 
--- devwp/src/js/custom-scripts.js
+++ devwp/src/js/custom-scripts.js
@@ -33,4 +33,32 @@ jQuery(document).ready(function() {
     });
 });
 
+
+jQuery(window).on('load', function() {
+var filter = document.getElementById('filter');
+var list = document.getElementById('list');
+var listItems = list.querySelectorAll('li');
+
+//filter.focus();
+
+filter.addEventListener('keyup', function(e) {
+    var val = new RegExp(e.target.value, 'gi');
+    for(var i=0; i<listItems.length; i++) {
+        if( e.target.value.length > 0) {
+            var text = listItems[i].innerHTML;
+
+            if( !text.match(val)) {
+                listItems[i].classList.add('is-hidden');
+            } else {
+                listItems[i].classList.remove('is-hidden');
+            }
+        } else {
+            listItems[i].classList.remove('is-hidden');
+        }
+
+    }
+});
+});
+
+
 console.log("Custom is linked properly fo sho yo 2");
